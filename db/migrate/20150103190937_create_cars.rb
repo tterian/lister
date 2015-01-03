6d3c55310860fefa45f1b792307987bb3d81cbc0
Tigran Terian <tigran.terian@gmail.com>
2015-01-03 23:15:24 +0400

    Generated cars model with correct data types

---
+++ db/migrate/20150103190937_create_cars.rb
@@ -0,0 +1,28 @@
+class CreateCars < ActiveRecord::Migration
+  def change
+    create_table :cars do |t|
+      t.integer :make
+      t.references :user, index: true
+      t.integer :model
+      t.datetime :year
+      t.integer :mileage
+      t.integer :price
+      t.string :location
+      t.string :picture
+      t.integer :fuel
+      t.integer :body
+      t.integer :drive
+      t.integer :hand
+      t.integer :door
+      t.string :engine
+      t.integer :transmission
+      t.string :color
+      t.string :interior
+      t.text :note
+
+      t.timestamps null: false
+    end
+    add_foreign_key :cars, :users
+    add_index :cars, [:user_id, :created_at]
+  end
+end
